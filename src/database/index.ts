d543572721f73941d0f14178ab354ec121e424f1
shivampython <shivammishra.python@gmail.com>
2024-10-09 16:22:20 +0530

    connection url changes

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const connectToDb=async()=>{
-const connectionURL=""
+const connectionURL=`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.awucexv.mongodb.net/`
 mongoose.connect(connectionURL).then(()=>{
     console.log('Database Connection Successful');
 }).catch(()=>{
